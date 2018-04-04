c2fec27746552a5bb5d73bfafdb6993829dea195
xyoTick <michal.parafiniuk@gmail.com>
2018-04-04 11:26:29 +0200

    remove default table-striped and table-hover classes

--- src/Table/TableFrame.vue
+++ src/Table/TableFrame.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table table-striped table-hover" style="margin-bottom: 0" :class="tblClass" :style="tblStyle">
+  <table class="table" style="margin-bottom: 0" :class="tblClass" :style="tblStyle">
     <colgroup>
       <col v-if="shouldRenderSelection" style="width: 30px"></col>
       <col v-for="col in columns" :class="col.colClass" :style="col.colStyle"></col>
